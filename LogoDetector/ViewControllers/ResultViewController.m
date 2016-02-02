050b5cb82091eac7bb7bf664f93b766fe597b096
Frederic Lemaitre <fredericlemaitre@mac-air.groupevsc.com>
2016-02-02 18:34:05 +0100

    habillage

--- LogoDetector/ViewControllers/ResultViewController.m
+++ LogoDetector/ViewControllers/ResultViewController.m
@@ -16,4 +16,11 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (IBAction)goToBook:(id)sender {
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://m.sejour.voyages-sncf.com/offre/sejour-malte/la-valette/seabank-4-etoiles-sup/id,442240/"]];
+
+}
+
+
 @end
